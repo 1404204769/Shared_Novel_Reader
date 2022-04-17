472d72f34feace74c4e6f6d3508f9ad42b88157d
xiloya <LZC20000318@163.com>
2022-04-17 23:15:48 +0800

    增加MyJson工具类, 在其中实现了读写Json文件的函数,检查Json对象字段是否存在类型是否正确的函数 增加Novel_Analysis工具类， 在其中设置了许多正则表达式， 在其中实现了读取本地资源文件(限制为txt版本)为Book类型的函数，实现了获取标题的函数，实现了读取字符串中的数字(无论是中文字符串还是阿拉伯字符串)的函数

---
+++ Shared_Novel_Reader/Tools/MyJson.cs
@@ -0,0 +1,195 @@
+﻿using log4net;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shared_Novel_Reader.Tools
+{
+    internal class MyJson
+    {
+        private static Regex FileTypeRegex = new Regex(@"[^<>/\\\|:""\*\?]+\.(?<FileType>\w+)$");
+        
+        /// <summary>
+        /// 从Json文件中解析Json对象
+        /// </summary>
+        /// <param name="obj">引用的Json对象，需要提前实例化</param>
+        /// <param name="path">需要解析的文件的地址</param>
+        /// <returns></returns>
+        public static bool JsonFromFile(ref JObject obj, string path)
+        {
+            string err;
+            ILog log = LogManager.GetLogger(typeof(MyJson));
+            if (String.IsNullOrEmpty(path))
+            {
+                err = "要解析的文件路径为空,请重新选择文件";
+                log.Info(err);
+                return false;
+            }
+            Match match = FileTypeRegex.Match(path);
+            if(!match.Success)
+            {
+                err = "要解析的文件路径错误,这不是规范的文件";
+                log.Info(err);
+                return false;
+            }
+            if(match.Groups["FileType"].Value != "json")
+            {
+                err = "要解析的文件路径错误,这不是Json文件";
+                log.Info(err);
+                return false;
+            }
+
+            try
+            {
+                FileStream fs = File.Open(path, FileMode.Open, FileAccess.Read);
+
+                StreamReader sr = new StreamReader(fs);
+
+                obj = JObject.Parse(sr.ReadToEnd());
+
+                sr.Close();// 关闭sr 释放资源
+            }
+            catch (SystemException ex)
+            {
+                err = "从文件解析Json发生SystemException异常";
+                log.Info(err, ex);
+                return false;
+            }
+            catch (Exception ex)
+            {
+                err = "从文件解析Json发生Exception异常";
+                log.Info(err, ex);
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 将Json对象写入文件中
+        /// </summary>
+        /// <param name="obj">要写入文件的Json对象</param>
+        /// <param name="path">要写入的路径(类似 C:\xxx\yy.json 这种格式)</param>
+        /// <returns></returns>
+        public static bool JsonToFile(in JObject obj, string path)
+        {
+            string err;
+            ILog log = LogManager.GetLogger(typeof(MyJson));
+            if (String.IsNullOrEmpty(path))
+            {
+                err = "保存文件的路径为空,请重新输入保存路径";
+                log.Info(err);
+                return false;
+            }
+            Match match = FileTypeRegex.Match(path);
+            if (!match.Success)
+            {
+                err = "保存文件的路径错误,这不是规范的(C:\\xxx\\yyy.json)路径";
+                log.Info(err);
+                return false;
+            }
+            if (match.Groups["FileType"].Value != "json")
+            {
+                err = "要保存的文件类型错误,这不是Json文件";
+                log.Info(err);
+                return false;
+            }
+
+            try
+            {
+                File.WriteAllText(path, obj.ToString(Newtonsoft.Json.Formatting.Indented, null), System.Text.Encoding.UTF8);//将内容写进json文件中
+            }
+            catch (SystemException ex)
+            {
+                err = "把Json写入文件发生SystemException异常";
+                log.Info(err, ex);
+                return false;
+            }
+            catch (Exception ex)
+            {
+                err = "把Json写入文件发生Exception异常";
+                log.Info(err, ex);
+                return false;
+            }
+            return true;
+        }
+
+
+
+
+        public static bool CheckColAndType(in JObject obj, string ColName,JTokenType tokenType)
+        {
+            string err;
+            ILog log = LogManager.GetLogger(typeof(MyJson));
+            JProperty jProperty = obj.Property(ColName);
+            if (jProperty == null)
+            {
+                log.Warn( ColName + "字段不存在");
+                return false;
+            }
+            JToken jToken = jProperty.Value;
+            if (jToken.Type != tokenType)
+            {
+                err = ColName + "字段不是";
+                switch (tokenType)
+                {
+                    case JTokenType.Object:
+                        {
+                            err += "Object类型";
+                        }
+                        break;
+                    case JTokenType.Array:
+                        {
+                            err += "Array类型";
+                        }
+                        break;
+                    case JTokenType.Float:
+                        {
+                            err += "Float类型";
+                        }
+                        break;
+                    case JTokenType.Integer:
+                        {
+                            err += "Integer类型";
+                        }
+                        break;
+                    case JTokenType.String:
+                        {
+                            err += "String类型";
+                        }
+                        break;
+                    case JTokenType.Boolean:
+                        {
+                            err += "Boolean类型";
+                        }
+                        break;
+                    case JTokenType.Date:
+                        {
+                            err += "Date类型";
+                        }
+                        break;
+                }
+                log.Warn(err);
+                return false;
+            }
+            return true;
+        }
+
+        public static bool CheckColAndTypeFromMap(in JObject obj, in Dictionary<string, JTokenType> map)
+        {
+            foreach (var VK in map)
+            {
+                if(!CheckColAndType(in obj,VK.Key,VK.Value))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Shared_Novel_Reader/Tools/Novel_Analysis.cs
@@ -0,0 +1,187 @@
+﻿using log4net;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Shared_Novel_Reader.Tools
+{   
+    internal class Novel_Analysis
+    {
+        private static Tools.Dependence.ChineseNumberConvert.ChineseNumberConverter CNC = new Tools.Dependence.ChineseNumberConvert.ChineseNumberConverter();
+        
+        private static Regex TitleRegex = new Regex(@"(?:^\s*|^\s*第.*?)(第[^\s,.，。]*?[章篇]\s?.*)");
+        private static Regex PartRegex = new Regex(@"(第[^\s,.，。]*?卷)");
+        private static Regex ChapterRegex = new Regex(@"(第([^\s,.，。]*?)[章篇])");
+        private static Regex ChineseNumRegex = new Regex(@"([一二三四五六七八九十百千万亿]+)");
+        private static Regex AlabNumRegex = new Regex(@"([0-9]+)");
+        private static Regex BookNameRegex = new Regex(@"(?<BookName>[^<>/\\\|:""\*\?]+)\.\w+$");
+        private static String Text { get; set; }// 内容
+
+        /// <summary>
+        /// 加载本地资源文件并解析
+        /// </summary>
+        /// <param name="path">要解析的文件的地址</param>
+        public static bool Analysis_Local_Resource(out models.Book newBook, String path)
+        {
+            string err;
+            ILog log = LogManager.GetLogger(typeof(models.Book));
+            if (String.IsNullOrEmpty(path))
+            {
+                newBook = null;
+                err = "要解析的文件路径为空,请重新选择文件";
+                log.Info(err);
+                return false;
+            }
+            try
+            {
+                FileStream fs = File.Open(path, FileMode.Open, FileAccess.Read);
+
+                StreamReader sr = new StreamReader(fs);
+                if (sr.CurrentEncoding == Encoding.UTF8)
+                {
+                    var chArr = new char[1024];
+                    sr.Read(chArr, 0, chArr.Length);
+                    var buffer1 = Encoding.UTF8.GetBytes(chArr);// 将chArr转化为UTF8编码的字节数组，单个字节处理
+                    var buffer2 = new byte[buffer1.Length];
+                    fs.Position = 0;
+                    fs.Read(buffer2, 0, buffer2.Length);
+                    var same = true;// 循环比较是否是UTF8编码，若不是则用GBK重新读取
+                    for (int i = 0; i < buffer1.Length; i++)
+                    {
+                        if (buffer1[i] != buffer2[i])
+                        {
+                            same = false;
+                            break;
+                        }
+                    }
+                    if (!same)
+                    {
+                        fs.Position = 0;
+                        sr = new StreamReader(fs, Encoding.GetEncoding("GBK"));
+                    }
+                    else
+                    {
+                        fs.Position = 0;
+                        sr = new StreamReader(fs);
+                    }
+                }
+                String line;
+                // 读取txt文件并分段保存，将章节名标记为isHeader
+
+                int num = 0, vol = 1;// 设置全局章节/卷标记
+                // 初始化新书
+                newBook = new models.Book();
+                newBook.File_Path.Add(fs.Name);
+                Match match = BookNameRegex.Match(fs.Name);
+                newBook.Book_Name = match.Groups["BookName"].ToString();
+            
+
+                // 初始化卷
+                models.Vol newVol = new models.Vol();
+                newVol.Vol_Num = vol;
+
+
+                // 初始化章节
+                models.Chapter newChapter = new models.Chapter();
+
+
+                // 初始化章节内容
+                models.Content newContent = new models.Content();
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (!String.IsNullOrWhiteSpace(line))// 读取新的一行
+                    {
+                        Text = line.Trim();// 格式化段落
+
+                        Match matchResult = TitleRegex.Match(Text);
+
+                        if (matchResult.Success)
+                        {
+                            Console.WriteLine(Text);
+                            // 如果是章节名,保存之前章节的数据并新建一个章节
+                            long chapnum = Novel_Analysis.GetNum(Text);
+                            if(num > 0)
+                            {
+                                newChapter.Push_Content(newContent);// 保存章节内容到Chapter中
+                                newContent = new models.Content();// 初始化章节内容
+                                newVol.Push_Chapter(newChapter);// 保存章节信息到卷中
+                                newChapter = new models.Chapter();// 初始化章节信息
+                            }
+                            newChapter.ChapTitle = Text;// 设置标题
+                            if (chapnum == 1 && num > 0)
+                            {
+                                // 说明开了新的一卷
+                                newBook.Push_Vol(newVol);// 保存卷信息到书中
+                                newVol = new models.Vol();// 初始化卷信息
+                                newVol.Vol_Num = ++vol;// 设置卷数为最新
+                            }
+                            num++;
+                        }
+                        else
+                        {
+                            // 不是章节名说明是章节内容，则保存到章节中
+                            newContent.ContentArray.Add(Text);// 保存段落内容到章节内容空间中
+                        }
+                    }
+                }
+
+                newBook.Push_Vol(newVol);// 保存卷信息到书中
+                sr.Dispose();// 关闭sr 释放资源
+            }
+            catch (SystemException ex)
+            {
+                newBook = null;
+                err = "解析本地资源发生异常";
+                log.Info(err,ex);
+                return false;
+            }
+            catch (Exception ex)
+            {
+                newBook = null;
+                err = "解析本地资源发生异常";
+                log.Info(err,ex);
+                return false;
+            }
+            return true;
+        }
+
+
+        
+        /// <summary>
+        /// 返回标题的第一个匹配项
+        /// </summary>
+        /// <param name="text"> 匹配对象 </param>
+        /// <returns></returns>
+        public static Match GetTitle(string text)
+        {
+            return TitleRegex.Match(text);
+        }
+
+        /// <summary>
+        /// 根据传入的字符串(全是中文数字/全是阿拉伯数字)返回整形阿拉伯数字
+        /// </summary>
+        /// <param name="text">要解析的字符串数字</param>
+        /// <returns>解析后的整形数字</returns>
+        public static long GetNum(string strNum)
+        {
+            Match ArabMatch = AlabNumRegex.Match(strNum);
+            if(ArabMatch.Success)
+            {
+                return Convert.ToInt64(ArabMatch.Value);
+            }
+            Match ChineseMatch = ChineseNumRegex.Match(strNum);
+            if (ChineseMatch.Success)
+            {
+                return CNC.Convert(ChineseMatch.Value);
+            }
+            return -1;
+        }
+
+    }
+}
